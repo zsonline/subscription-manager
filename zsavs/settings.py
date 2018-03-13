d4615c0007071239b51fc8989c73132ad9a789e0
Robin Bisping <robin@bisping.ch>
2018-03-13 19:55:14 +0100

    Fixes duplicates in app registration

--- zsavs/settings.py
+++ zsavs/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'authentication',
-    'subscription',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'subscription.apps.SubscriptionConfig',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
