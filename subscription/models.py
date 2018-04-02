6711c964c86661151ab2483c7d1b19a8df4f66c7
Robin Bisping <robin@bisping.ch>
2018-04-02 16:54:08 +0200

    Adds address model in subscription app

--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+
+class Address(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    street_1 = models.CharField(max_length=50)
+    street_2 = models.CharField(max_length=50, blank=True, null=True)
+    postcode = models.CharField(max_length=8)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
