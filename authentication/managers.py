1c2e588ceb167484ca199e81172d0a611795056d
Robin Bisping <robin@bisping.ch>
2018-04-04 18:39:53 +0200

    Fixes unique token constraint. If a token throws an integrity error, a new one is created.

--- authentication/managers.py
+++ authentication/managers.py
@@ -1,7 +1,6 @@
-from datetime import datetime, timedelta
-
+# Django imports
 from django.contrib.auth.models import BaseUserManager
-from django.db import models
+from django.db import models, IntegrityError
 from django.utils.crypto import get_random_string
 from django.conf import settings
 from django.utils import timezone
@@ -40,22 +39,47 @@ class UserManager(BaseUserManager):
 
 
 class LoginTokenManager(models.Manager):
+    """
+    Custom manager for login tokens.
+    """
 
     def create(self, **obj_data):
+        """
+        Overrides the default create method. It sets the valid_until
+        attribute and generates a random code.
+        """
+
+        # Set token expiration
         obj_data['valid_until'] = timezone.now() + settings.TOKEN_EXPIRATION
-        obj_data['code'] = get_random_string(settings.TOKEN_LENGTH)
-        return super().create(**obj_data)
+
+        # Set code and create LoginToken object
+        while True:
+            # Generate a random token
+            obj_data['code'] = get_random_string(settings.TOKEN_LENGTH)
+            # Try creating the object
+            try:
+                token = super().create(**obj_data)
+            # If token already exists, generate a new one and try again
+            except IntegrityError:
+                continue
+            # Token successfully created, return
+            break
+
+        return token
 
     def create_and_send(self, **obj_data):
+        """
+        Creates and sends a token object.
+        """
+
         token = self.create(**obj_data)
-        token.save()
         token.send()
         return token
 
     def all_expired(self):
         """Selects all tokens that have been sent more than seven days ago"""
         return self.filter(
-            sent_at__lt=datetime.now() - timedelta(days=settings.EMAIL_VERIFICATION_TOKEN_EXPIRATION_DAYS)
+            sent_at__lt=timezone.now() - settings.TOKEN_EXPIRATION
         )
 
     def delete_all_expired(self):
