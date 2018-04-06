9a947c96d08a363499aaec3d573f12997ea66138
Robin Bisping <robin@bisping.ch>
2018-04-06 15:21:47 +0200

    Adds redirect from auth/ to auth/login/

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,12 @@
+# Django imports
 from django.urls import path
+from django.views.generic import RedirectView
 
+# Application imports
 from .views import signup_view, login_view, logout_view, LoginTokenView, home_view
 
-
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('token/', LoginTokenView.as_view(), name='verify_token'),
