b7c77c6f12acb771b216d47710ab44189757e47c
Robin Bisping <robin@bisping.ch>
2018-04-23 17:19:36 +0200

    Improves subscription models and registers SubscriptionType in admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Subscription, SubscriptionType, Address
 
-# Register your models here.
+
+class SubscriptionTypeAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'slug', 'duration', 'price')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('email_whitelist',),
+        }),
+    )
+
+
+admin.site.register(SubscriptionType, SubscriptionTypeAdmin)
--- subscription/models.py
+++ subscription/models.py
@@ -1,6 +1,7 @@
 # Django imports
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class Subscription(models.Model):
@@ -18,11 +19,13 @@ class Subscription(models.Model):
     )
     shipping_address = models.ForeignKey(
         'Address',
-        on_delete=models.PROTECT
+        on_delete=models.PROTECT,
+        related_name='shipping_address'
     )
     billing_address = models.ForeignKey(
         'Address',
-        on_delete=models.PROTECT
+        on_delete=models.PROTECT,
+        related_name='billing_address'
     )
     start_date = models.DateField()
     end_Date = models.DateField()
@@ -39,17 +42,20 @@ class SubscriptionType(models.Model):
     name = models.CharField(
         max_length=50
     )
-    description = models.CharField(
-        max_length=500
+    description = models.TextField()
+    slug = models.SlugField(
+        unique=True,
+        help_text=_('Unique identifier for this subscription type.')
     )
-    slug = models.SlugField()
-    duration = models.DurationField()
-    price = models.IntegerField()
+    duration = models.DurationField(
+        help_text=_('Specify a duration in the format \"years:months:days\".')
+    )
+    price = models.IntegerField(
+        help_text=_('Price in CHF for the duration.')
+    )
     # Whitelist for email addresses that are allowed
     # for a certain subscription type
-    email_whitelist = models.CharField(
-        max_length=500
-    )
+    email_whitelist = models.TextField()
 
 
 class Address(models.Model):
@@ -58,16 +64,19 @@ class Address(models.Model):
     """
     first_name = models.CharField(
         max_length=30,
-        blank=True
+        blank=True,
+        default=''
     )
     last_name = models.CharField(
         max_length=150,
-        blank=True
+        blank=True,
+        default=''
     )
     street_1 = models.CharField(max_length=50)
     street_2 = models.CharField(
         max_length=50,
-        blank=True
+        blank=True,
+        default=''
     )
     postcode = models.CharField(max_length=8)
     city = models.CharField(max_length=50)
