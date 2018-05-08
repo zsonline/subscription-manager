22f50307daf13d8a3267fa0f4c45153cdab08db5
Robin Bisping <robin@bisping.ch>
2018-05-08 19:58:02 +0200

    Comments email backend for login tokens

--- authentication/backends.py
+++ authentication/backends.py
@@ -6,6 +6,7 @@ from django.contrib.auth.backends import ModelBackend
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.utils.translation import gettext_lazy as _
 
 # Application imports
 from .models import LoginToken
@@ -41,22 +42,36 @@ class LoginTokenBackend(ModelBackend):
 
 
 class EmailBackend(LoginTokenBackend):
-
+    """
+    Inherits LoginTokenBackend class and adds
+    a send method that sends tokens via email.
+    """
     @staticmethod
     def send(token, code):
-        subject = 'Subject'
-        to_email = [token.user.email]
-        from_email = settings.EMAIL_FROM_ADDRESS
-
+        """
+        Creates and sends a login token email for a
+        given token object and a code. It makes a .txt
+        and a .html version of the email.
+        """
+        # Make a context variable for the templates
         context = {
             'to_name': token.user.first_name,
-            'from_name': settings.NAME,
+            'from_name': settings.ORGANISATION_NAME,
             'url': LoginToken.login_url(token.user.email, code),
         }
-        text_content = render_to_string('authentication/emails/token_email.txt', context)
-        html_content = render_to_string('authentication/emails/token_email.html', context)
-
-        message = EmailMultiAlternatives(subject, text_content, from_email, to_email)
+        # Render the content templates
+        text_content = render_to_string('authentication/emails/login_token.txt', context)
+        html_content = render_to_string('authentication/emails/login_token.html', context)
+        # Create the text and html version of the email
+        message = EmailMultiAlternatives(
+            subject=_('Login Token'),
+            body=text_content,
+            from_email=settings.ORGANISATION_FROM_EMAIL,
+            to=[token.user.email],
+            headers={
+                'Reply-To': settings.ORGANISATION_REPLY_TO_EMAIL
+            }
+        )
         message.attach_alternative(html_content, 'text/html')
-
+        # Send the email
         message.send()
