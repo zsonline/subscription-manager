63178861bf69752df4aedbdae98ed1a56372ee0d
Robin Bisping <robin@bisping.ch>
2018-05-15 22:07:30 +0200

    Removes payment type

--- subscription_manager/payment/models.py
+++ subscription_manager/payment/models.py
@@ -5,20 +5,15 @@ from django.utils.translation import gettext_lazy as _
 
 class Payment(models.Model):
     amount = models.IntegerField()
-    type = models.CharField(
-        max_length=7,
-        choices=(
-            ('invoice', _('Invoice')),
-            ('card', _('Credit card')),
-            ('free', _('Free'))
-        )
-    )
     paid_at = models.DateTimeField(
         blank=True,
         null=True,
         default=None
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Payment({}, {}, {})'.format(self.subscription.user.name, self.amount, self.paid_at)
 
     def is_paid(self):
         return self.paid_at is not None
