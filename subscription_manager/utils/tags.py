6e8553d8b69fbb5dd59eac6c1fa9ad459157a45f
Robin Bisping <robin@bisping.ch>
2018-05-17 09:55:34 +0200

    Improves active tag, looks for substrings

--- subscription_manager/utils/tags.py
+++ subscription_manager/utils/tags.py
@@ -8,15 +8,16 @@ from django.shortcuts import reverse
 register = template.Library()
 
 
-@register.simple_tag
-def active(request, view_name):
+@register.simple_tag(takes_context=True)
+def active(context, view_name):
     """
     Tag function that compares the current url
     with the path of a given view name. If they
     are equal, the string 'active' is returned.
     Otherwise an empty string.
     """
-    if reverse(view_name) == request.path:
+    request = context['request']
+    if reverse(view_name) in request.path:
         return 'active'
     return ''
 
