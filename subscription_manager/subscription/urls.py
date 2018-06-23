f65ae20c9029210a433b27679894e12e536d68ae
Robin Bisping <robin@bisping.ch>
2018-06-23 14:00:39 +0200

    Better urls

--- subscription_manager/subscription/urls.py
+++ subscription_manager/subscription/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     #path('subscription/<plan_slug>/', purchase_view, name='purchase'),
     path('home/', RedirectView.as_view(pattern_name='subscription_list', permanent=True), name='login_index'),
     path('subscription/', SubscriptionListView.as_view(), name='subscription_list'),
-    path('subscription/add/', PlanListView.as_view(), name='plan_list'),
-    path('subscription/add/<plan_slug>/', SubscriptionCreateView.as_view(), name='subscription_create'),
+    path('subscription/buy/', PlanListView.as_view(), name='plan_list'),
+    path('subscription/buy/<plan_slug>/', SubscriptionCreateView.as_view(), name='subscription_create'),
     path('subscription/<int:subscription_id>/', SubscriptionDetailView.as_view(), name='subscription_detail'),
     path('subscription/<int:subscription_id>/edit/', SubscriptionUpdateView.as_view(), name='subscription_update'),
-    path('subscription/<int:subscription_id>/delete/', SubscriptionDeleteView.as_view(), name='subscription_delete')
+    path('subscription/<int:subscription_id>/cancel/', SubscriptionDeleteView.as_view(), name='subscription_delete')
 ]
