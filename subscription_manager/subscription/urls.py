c9d2236bdb65eee460864e68347dcd29f8c0c166
Robin Bisping <robin@bisping.ch>
2018-07-07 15:11:41 +0200

    Removes external purchase views

--- subscription_manager/subscription/urls.py
+++ subscription_manager/subscription/urls.py
@@ -3,13 +3,11 @@ from django.urls import path
 from django.views.generic.base import RedirectView
 
 # Application imports
-from .views import plan_list_view, purchase_view, SubscriptionListView, SubscriptionCreateView, \
-    SubscriptionUpdateView, SubscriptionDetailView, SubscriptionCancelView, PlanListView
+from .views import SubscriptionListView, SubscriptionCreateView, SubscriptionUpdateView, SubscriptionDetailView,\
+    SubscriptionCancelView, PlanListView
 
 # URL patterns
 urlpatterns = [
-    #path('subscriptions/', plan_list_view, name='plans'),
-    #path('subscription/<plan_slug>/', purchase_view, name='purchase'),
     path('home/', RedirectView.as_view(pattern_name='subscription_list', permanent=True), name='login_index'),
     path('abo/', SubscriptionListView.as_view(), name='subscription_list'),
     path('abo/kaufen/', PlanListView.as_view(), name='plan_list'),
