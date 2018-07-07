1b5a398174e298e9f2b53c88911b5736397f9586
Robin Bisping <robin@bisping.ch>
2018-07-07 14:50:40 +0200

    German routes

--- subscription_manager/subscription/urls.py
+++ subscription_manager/subscription/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     #path('subscriptions/', plan_list_view, name='plans'),
     #path('subscription/<plan_slug>/', purchase_view, name='purchase'),
     path('home/', RedirectView.as_view(pattern_name='subscription_list', permanent=True), name='login_index'),
-    path('subscription/', SubscriptionListView.as_view(), name='subscription_list'),
-    path('subscription/buy/', PlanListView.as_view(), name='plan_list'),
-    path('subscription/buy/<plan_slug>/', SubscriptionCreateView.as_view(), name='subscription_create'),
-    path('subscription/<int:subscription_id>/', SubscriptionDetailView.as_view(), name='subscription_detail'),
-    path('subscription/<int:subscription_id>/edit/', SubscriptionUpdateView.as_view(), name='subscription_update'),
-    path('subscription/<int:subscription_id>/cancel/', SubscriptionCancelView.as_view(), name='subscription_cancel')
+    path('abo/', SubscriptionListView.as_view(), name='subscription_list'),
+    path('abo/kaufen/', PlanListView.as_view(), name='plan_list'),
+    path('abo/kaufen/<plan_slug>/', SubscriptionCreateView.as_view(), name='subscription_create'),
+    path('abo/<int:subscription_id>/', SubscriptionDetailView.as_view(), name='subscription_detail'),
+    path('abo/<int:subscription_id>/bearbeiten/', SubscriptionUpdateView.as_view(), name='subscription_update'),
+    path('abo/<int:subscription_id>/kuendigen/', SubscriptionCancelView.as_view(), name='subscription_cancel')
 ]
