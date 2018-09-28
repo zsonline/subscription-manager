9605b4094c93e81ef7de5284252988f7f1b288d3
Robin Bisping <robin@bisping.ch>
2018-09-28 13:40:42 +0200

    Improves and styles http errors

--- subscription_manager/core/urls.py
+++ subscription_manager/core/urls.py
@@ -1,7 +1,9 @@
 # Django imports
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+from django.views.defaults import page_not_found, server_error
+from django.views.generic.base import RedirectView, TemplateView
 
 urlpatterns = [
     path('', include('subscription_manager.landing.urls')),
@@ -10,3 +12,11 @@ urlpatterns = [
     path('auth/', include('subscription_manager.authentication.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('400/', TemplateView.as_view(template_name='400.html')),
+        path('403/', TemplateView.as_view(template_name='403.html')),
+        path('404/', TemplateView.as_view(template_name='404.html')),
+        path('500/', TemplateView.as_view(template_name='500.html')),
+    ]
