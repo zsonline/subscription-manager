8b1927a587970943554b32c1f37ae8bc5517541f
Robin Bisping <robin@bisping.ch>
2018-09-30 16:53:48 +0200

    Adds error views and disables templatetag

--- subscription_manager/landing/urls.py
+++ subscription_manager/landing/urls.py
@@ -1,5 +1,6 @@
 # Django imports
 from django.urls import path
+from django.views.generic import TemplateView
 
 # Application imports
 from .views import PlanListView
@@ -8,3 +9,8 @@ from .views import PlanListView
 urlpatterns = [
     path('', PlanListView.as_view(), name='index'),
 ]
+
+handler400 = TemplateView.as_view(template_name='400.html')
+handler403 = TemplateView.as_view(template_name='403.html')
+handler404 = TemplateView.as_view(template_name='404.html')
+handler500 = TemplateView.as_view(template_name='500.html')
