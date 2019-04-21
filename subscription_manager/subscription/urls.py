1c0c1c17a2ceecd1120bf07b38447190101e4237
Robin Bisping <robin@bisping.ch>
2019-04-21 15:46:57 +0200

    Change links for better navigation highlighting of active items

--- subscription_manager/subscription/urls.py
+++ subscription_manager/subscription/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('bestellen/', PlanListView.as_view(), name='plan_list'),
     path('bestellen/<slug:plan_slug>/', SubscriptionCreateView.as_view(), name='subscription_create'),
     path('abos/', SubscriptionListView.as_view(), name='subscription_list'),
-    path('abo/abonnieren/<plan_slug>/', SubscriptionCreateView.as_view(), name='subscription_create1'),
-    path('abo/<int:subscription_id>/', SubscriptionDetailView.as_view(), name='subscription_detail'),
-    path('abo/<int:subscription_id>/bearbeiten/', SubscriptionUpdateView.as_view(), name='subscription_update'),
-    path('abo/<int:subscription_id>/kuendigen/', SubscriptionCancelView.as_view(), name='subscription_cancel')
+    path('abos/<int:subscription_id>/', SubscriptionDetailView.as_view(), name='subscription_detail'),
+    path('abos/<int:subscription_id>/bearbeiten/', SubscriptionUpdateView.as_view(), name='subscription_update'),
+    path('abos/<int:subscription_id>/kündigen/', SubscriptionCancelView.as_view(), name='subscription_cancel')
 ]
