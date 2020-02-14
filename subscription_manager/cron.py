578fcb412d96083e409528ad2a752c2c00720538
Robin Bisping <robin@bisping.ch>
2020-02-15 00:25:05 +0100

    Add cron tasks

---
+++ subscription_manager/cron.py
@@ -0,0 +1,33 @@
+from django_cron import CronJobBase, Schedule
+
+from django.core.management import call_command
+
+from subscription_manager.subscription.models import Subscription
+from subscription_manager.user.models import Token
+
+
+class SendEmails(CronJobBase):
+    schedule = Schedule(run_at_times=['07:00'])
+    code = 'send_emails'
+
+    def do(self):
+        """
+        Send notification emails to users whose subscriptions
+        are expiring within 30 days or whose subscription has
+        ended.
+        """
+        Subscription.objects.send_expiration_emails(30)
+        Subscription.objects.send_expiration_emails(1)
+
+
+class CleanDatabase(CronJobBase):
+    schedule = Schedule(run_at_times=['04:00'])
+    code = 'clean_database'
+
+    def do(self):
+        """
+        Clear expired sessions, remove expired tokens
+        and clean email log each day at 4 am.
+        """
+        call_command('clearsessions', '--verbosity=0')
+        Token.objects.all_expired().delete()
