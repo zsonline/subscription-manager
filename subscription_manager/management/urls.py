db7f6c956c201e9677af4ffdce4c672ef4fa83f1
Robin Bisping <robin@bisping.ch>
2020-02-17 23:09:13 +0100

    Add management overview page

--- subscription_manager/management/urls.py
+++ subscription_manager/management/urls.py
@@ -3,6 +3,9 @@ from django.views.generic.base import RedirectView
 
 from subscription_manager.subscription.views import SubscriptionExportView
 
+from .views import ManagementListView
+
 urlpatterns = [
+    path('', ManagementListView.as_view(), name='management_list'),
     path('exportieren/<str:format>/', SubscriptionExportView.as_view(), name='subscriptions_export'),
 ]
--- subscription_manager/management/views.py
+++ subscription_manager/management/views.py
@@ -1,2 +1,9 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, HttpResponse, Http404
-from django.views.generic import View
+from django.utils.decorators import method_decorator
+from django.views.generic import View, TemplateView
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class ManagementListView(TemplateView):
+    template_name = 'management/management_list.html'
