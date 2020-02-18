c479926556a33e035bfefb66c5b014d7a9fdc720
Robin Bisping <robin@bisping.ch>
2020-02-18 20:28:56 +0100

    Load .env variables into environment with django-dotenv

--- subscription_manager/wsgi.py
+++ subscription_manager/wsgi.py
@@ -1,8 +1,13 @@
 import os
 
+import dotenv
+
 from django.core.wsgi import get_wsgi_application
 
 # Load production settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'subscription_manager.settings.production')
+
+# Load .env variables
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 application = get_wsgi_application()
