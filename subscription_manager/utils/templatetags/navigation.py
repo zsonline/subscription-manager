009626494287df02f2c84a54ed49b036ba8c8594
Robin Bisping <robin@bisping.ch>
2020-03-09 19:42:57 +0100

    Catch missing request object on error pages

--- subscription_manager/utils/templatetags/navigation.py
+++ subscription_manager/utils/templatetags/navigation.py
@@ -11,7 +11,10 @@ def active(context, *view_names):
     requested view name. If at least one is equal, 'active'
     is returned. Otherwise an empty string.
     """
-    request = context['request']
+    request = context.get('request')
+    if request is None:
+        return ''
+
     for view_name in view_names:
         if request.resolver_match.url_name == view_name:
             return 'active'
